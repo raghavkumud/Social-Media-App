10a119c0c02a3215cc9a159a42cf04c6ff2155f7
raghavkumud <kumud544131@gmail.com>
2022-07-25 13:05:09 +0530

    changed mail credentials for sending reset password link in sendEmail.js

--- backend/middleware/sendEmail.js
+++ backend/middleware/sendEmail.js
@@ -1,22 +1,13 @@
 const nodeMailer = require("nodemailer");
 exports.sendEmail = async (options) => {
-  /*const transporter = nodeMailer.createTransport({
-    host: process.env.SMPT.HOST,
-    port: process.env.SMPT_PORT,
-    auth: {
-        user: process.env.SMPT_MAIL,
-        pass: process.env.SMPT_PASSWORD
-    }, 
-    service: process.env.SMPT_SERVICE,
-  });*/
   var transport = nodeMailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "8b0a1658410ba5",
-    pass: "57b6af2c2ba510"
-  }
-});
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "0f11fd1a79e6cb",
+      pass: "59b052df46f277",
+    },
+  });
 
   const mailOptions = {
     from: process.env.SMPT_MAIL,
