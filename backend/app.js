8a54e39ba38bd0987a610fafcafccbe3a7ca3ef9
raghavkumud <kumud544131@gmail.com>
2023-02-07 17:50:07 +0530

    modified backend/index.js

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,7 @@ const path = require("path");
 
 app.use(compression());
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config({ path: __dirname + "/config/config.env" });
-}
+require("dotenv").config({ path: __dirname + "/config/config.env" });
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
